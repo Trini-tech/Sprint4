35e755f7fb2bbd91923ec795d198f4b4af5ddeab
trini-tech <trinimarco02@gmail.com>
2023-09-19 17:17:09 +0200

    Exercici2

--- src/films.js
+++ src/films.js
@@ -5,7 +5,10 @@ function getAllDirectors(movies) {
 }
 
 // Exercise 2: Get the films of a certain director
-function getMoviesFromDirector(array, director) {}
+function getMoviesFromDirector(movies, director) {
+  let result = movies.filter((movie) => movie.director === director);
+  return result;
+}
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {}

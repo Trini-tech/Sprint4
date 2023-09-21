698cff96bf680d62499483aca4ba012dc4b53ece
trini-tech <trinimarco02@gmail.com>
2023-09-21 12:30:18 +0200

    Exercici3

--- src/films.js
+++ src/films.js
@@ -1,32 +1,42 @@
 // Exercise 1: Get the array of all directors.
-function getAllDirectors(movies) {
-  let result = movies.map((movie) => movie.director);
+function getAllDirectors(array) {
+  let result = array.map((movie) => movie.director);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
-function getMoviesFromDirector(movies, director) {
-  let result = movies.filter((movie) => movie.director === director);
+function getMoviesFromDirector(array, director) {
+  let result = array.filter((movie) => movie.director === director);
   return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
-function moviesAverageOfDirector(array, director) {}
+function moviesAverageOfDirector(array, director) {
+  let moviesFromDirector = getMoviesFromDirector(array, director);
+  let avg = moviesFromDirector.reduce((prev, cur) => prev + cur.score,0)/moviesFromDirector.length;
+  return parseFloat(avg.toFixed(2));
+}
 
 // Exercise 4:  Alphabetic order by title
-function orderAlphabetically(array) {}
+function orderAlphabetically(array) {
+}
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {}
+function orderByYear(array) {
+}
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {}
+function moviesAverageByCategory(array, category) {
+}
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {}
+function hoursToMinutes(array) {
+}
 
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear() {}
+function bestFilmOfYear(array, year) {
+}
+
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
